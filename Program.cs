b7939552dd0e6675a27c8c83f371e6ef4077e14a
Tiago Grigull <tagrigull@gmail.com>
2024-09-23 21:05:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿//PERADORES ARITMéTRICOS
+
+//Questão 1
+//Console.WriteLine("Digite o primeiro número");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o segundo numero");
+//int num2 = int.Parse(Console.ReadLine());
+
+//int soma = num1 + num2;
+
+//Console.WriteLine($"O resultado da soma entre num1 e num2 é de:{soma}");
+
+//Questão 2
+//Console.WriteLine("Digite o primeiro número");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o segundo numero");
+//int num2 = int.Parse(Console.ReadLine());
+
+//int diferenca = num1 - num2;
+//Console.WriteLine($"a diferença entre numn1 e num2 é de:{diferenca}");
+
+//Questão 3
+//Console.WriteLine("Digite o primeiro número");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o segundo numero");
+//int num2 = int.Parse(Console.ReadLine());
+
+//int produto = num1 * num2;
+//Console.WriteLine($"O produto da multiplicação entre num1 e num2 é:{produto}");
+
+//Questão 4
+//Console.WriteLine("Digite o primeiro número");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o segundo numero");
+//int num2 = int.Parse(Console.ReadLine());
+
+//int divisao = num1/ num2;
+//Console.WriteLine($"O resultado da divisao de num1 e num2 é de:{divisao}");
+
+//Questão 5
+//Console.WriteLine("Digite o primeiro número");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o segundo numero");
+//int num2 = int.Parse(Console.ReadLine());
+//int resto = num1 % num2;
+//Console.WriteLine($"O resto da deivisão entre num1 e num2 é de:{resto}");
+
+
+//OPERADORES DE ATRIBUIÇÂO
+
+//Questão 1
+//int valor;
+//Console.WriteLine("escreva um número");
+//valor = int.Parse(Console.ReadLine());
+//Console.writeline($"o número que você digitou é {valor}");
+
+//Questão 2
+//int valor1 = 2;
+//Console.WriteLine("escreva um número");
+//int valor2 = int.Parse(Console.ReadLine());
+//valor1 += valor2;
+//Console.WriteLine($"o resultado é {valor1}");
+
+//Questão 3
+//int valor1 = 10;
+//Console.WriteLine("escreva um número");
+//int valor2 = int.Parse(Console.ReadLine());
+//valor1 -= valor2;
+//Console.WriteLine($"o resultado é {valor1}");
+
+//Questão 4
+//int valor1 = 10;
+//Console.WriteLine("escreva um número");
+//int valor2 = int.Parse(Console.ReadLine());
+//valor1 *= valor2;
+//Console.WriteLine($"o resultado é {valor1}");
+
+//Questão 5
+//int valor1 = 100;
+//Console.WriteLine("escreva um número");
+//int valor2 = int.Parse(Console.ReadLine());
+//valor1 /= valor2;
+//Console.WriteLine($"o resultado é {valor1}");
+
+
+//OPERADORES LÓGICOS
+
+//Questão 1
+//Console.WriteLine("Escreva dois numeros");
+//int num1 = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//if (num1>0 && num2 > 0)
+//{
+//    Console.WriteLine("Os dois numeros são maiores que zero");
+//}
+//else
+//{
+//    Console.WriteLine("OS numeros são menores que zero");
+//}
+
+//Questão 2
+//Console.WriteLine("Escreva dois numeros");
+//int num1 = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//if (num1 > 0 || num2 > 0)
+//{
+//    Console.WriteLine("Pelo menos um dos dois numeros é maior que zero");
+//}
+//else
+//{
+//    Console.WriteLine(" Pelo menos um dos dois numeros é menor que zero");
+//}
+
+//OPERADORES TERNÁRIOS
+
+//questão 1
+//Console.WriteLine("digite a sua idade");
+//int idade = int.Parse(Console.ReadLine());
+//Console.WriteLine($"você é {(idade >= 18 ? "maior de idade" : "menor de idade")} ");
+
+
+//Questão 2
+//Console.WriteLine("Digite um número");
+//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine($"O número é {(num % 2 == 0 ? "par" : "impar")}");
+
+
+//Questão 3
+//Console.WriteLine("Escreva um número");
+//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine($"O número é {(num > 0 ? "maior que zero" : num < 0 ? "menor que zero" : "igual a zero")}");
